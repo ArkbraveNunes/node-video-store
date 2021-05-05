c43341415e4556879eb1b9d1d6cc95bd74cd73aa
Jean Nunes <jean.santos@compasso.com.br>
2021-05-05 05:29:27 -0300

    Add script, update readme, update swagger, update .env and update package

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # node-video-store
 Video Store Node Application
+
+Desenvolvimento de uma api para uma Locadora de Filmes utilizando a tecnologia Node.JS e Mysql Database
+
+Requisitos:
+
+Node instalado (preferência versão lts);
+Ter o Docker instalado;
+Rodar o comando: `docker pull mysql` e baixar a imagem mysql mais recente;
+Rodar o container com o mysql `docker run --name some-mysql -e MYSQL_ROOT_PASSWORD=my-secret-pw -d mysql:tag`
+Ter o MySql Workbench instalado;
+Conectar com o mysql que esta rodando com o docker
+Executar o script localizado na pasta scripts.
+Rodar o comando "npm install" pra instalar dependências;
+Rodar o comando "npm start" para executar a aplicação;
--- src/launcher.js
+++ src/launcher.js
@@ -1,22 +1,30 @@
 require("custom-env").env(true);
+
 const express = require("express");
 
 const app = express();
+
 const helmet = require("helmet");
+const Logger = require("./utils/logger");
 const Router = require("./routes/routes");
+
+const Database = require("./database/database");
 
 module.exports = class Launcher {
   constructor() {
     this.port = process.env.PORT || 3000;
+    this.database = new Database();
+    app.use(express.json());
     app.use(helmet());
     app.use(Router.initialize());
   }
 
   start() {
+    this.database.authenticateConnection();
     app
       .listen(this.port, () =>
-        console.log(`Listening at http://localhost:${this.port}`)
+        Logger.info(`Listening at http://localhost:${this.port}`)
       )
-      .on("error", (err) => console.error(`Err: Error listen server: ${err}`));
+      .on("error", (err) => Logger.error(`Err: Error listen server: ${err}`));
   }
 };
